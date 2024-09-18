aba65fe9a1e271c0a151f6d1b4f6f02b2fa8b923
lbauch <bauchspiess.lucas@gmail.com>
2024-09-17 21:54:50 -0300

    inserindo novas classes e alterando erros

---
+++ Compiladores/src/controller/Constants.java
@@ -0,0 +1,43 @@
+package controller;
+
+public interface Constants extends ScannerConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_pr = 2;
+    int t_main = 3;
+    int t_end = 4;
+    int t_if = 5;
+    int t_elif = 6;
+    int t_else = 7;
+    int t_false = 8;
+    int t_true = 9;
+    int t_read = 10;
+    int t_write = 11;
+    int t_writeln = 12;
+    int t_repeat = 13;
+    int t_until = 14;
+    int t_while = 15;
+    int t_TOKEN_16 = 16; //"&&"
+    int t_TOKEN_17 = 17; //"||"
+    int t_TOKEN_18 = 18; //"!"
+    int t_TOKEN_19 = 19; //"=="
+    int t_TOKEN_20 = 20; //"!="
+    int t_TOKEN_21 = 21; //"<"
+    int t_TOKEN_22 = 22; //">"
+    int t_TOKEN_23 = 23; //"+"
+    int t_TOKEN_24 = 24; //"-"
+    int t_TOKEN_25 = 25; //"*"
+    int t_TOKEN_26 = 26; //"/"
+    int t_TOKEN_27 = 27; //","
+    int t_TOKEN_28 = 28; //";"
+    int t_TOKEN_29 = 29; //"="
+    int t_TOKEN_30 = 30; //"("
+    int t_TOKEN_31 = 31; //")"
+    int t_id = 32;
+    int t_cte_int = 33;
+    int t_cte_real = 34;
+    int t_cte_string = 35;
+
+}
